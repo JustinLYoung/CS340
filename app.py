b6a83d5aab7988984e6c7d1dc78ae088679dd78d
Justin Young <stinlloydyoung@gmail.comgit config --global user.email stinlloydyoung@gmail.com>
2024-03-05 08:59:08 -0800

    updated app.py with trainers, edit_trainers

--- app.py
+++ app.py
@@ -264,6 +264,91 @@ def edit_member(memberID):
 
     return redirect("/members")
 
+# Trainers
+
+# Citation for the following: trainers, edit_trainers
+# Date: 2/29/2024
+# Copied from: Github Flask Starter App
+# Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app/blob/master/bsg_people_app/app.py
+
+@app.route("/trainers", methods=["POST", "GET"])
+def trainers():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a person into the bsg_people entity
+    if request.method == "POST":
+        # fire off if user presses the Add Person button
+        if request.form.get("Add_Trainer"):
+            # grab user form inputs
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+
+            query = "INSERT INTO Trainers (firstName, lastName) VALUES (%s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (firstName, lastName))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/trainers")
+
+    # Grab bsg_people data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the people in bsg_people
+        query = "SELECT * FROM Trainers;"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("trainers.j2", data=data)
+
+
+# route for delete functionality, deleting a person from bsg_people,
+# we want to pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/delete_trainers/<int:id>")
+def delete_trainers(id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Trainers WHERE trainerID = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+    return redirect("/trainers")
+
+# route for edit functionality, updating the attributes of a person in bsg_people
+# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/edit_trainers/<int:id>", methods=["POST", "GET"])
+def edit_trainers(id):
+    if request.method == "GET":
+        # mySQL query to grab the info of the person with our passed id
+        query = "SELECT * FROM Trainers WHERE trainerID = %s" % (id)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("edit_trainers.j2", data=data)
+
+    # meat and potatoes of our update functionality
+    if request.method == "POST":
+        # fire off if user clicks the 'Edit Person' button
+        if request.form.get("edit_trainers"):
+            # grab user form inputs
+            trainerID = request.form["trainerID"]
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+
+            query = "UPDATE Trainers SET Trainers.firstName = %s, Trainers.lastName = %s WHERE Trainers.trainerID = %s"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (firstName, lastName, trainerID))
+            mysql.connection.commit()
+
+            # redirect back to people page after we execute the update query
+            return redirect("/trainers")
+
+
+
+
 # Listener
 if __name__ == "__main__":
 
